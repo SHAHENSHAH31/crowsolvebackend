67a8ade017b831e0e7b4161d1caa4b38f8066952
SHAHENSHAH31 <tyagichanchal407@gmail.com>
2025-09-01 14:46:24 +0530

    new

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173','https://crowdsolvefrontend.netlify.app'], credentials: true }));
 app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
 app.use(fileUpload());
